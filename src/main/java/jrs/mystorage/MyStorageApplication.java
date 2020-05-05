93e7fa7c0e63539ea1597e7b3dd97e08284342e4
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-05-05 23:35:20 +0200

    Create project

---
+++ src/main/java/jrs/mystorage/MyStorageApplication.java
@@ -0,0 +1,13 @@
+package jrs.mystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStorageApplication.class, args);
+    }
+
+}
