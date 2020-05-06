e2206165a0101f682d32e3c06792f673541d58f4
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-05-06 23:36:37 +0200

    Remove user and UserRepository.java

---
+++ src/main/java/jrs/mystorage/employee/model/Employee.java
@@ -0,0 +1,67 @@
+package jrs.mystorage.employee.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(insertable = false, updatable = false)
+    private UUID employeeId;
+
+    @Column(updatable = false, nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Length(max = 16)
+    @Column(length = 32, unique = true)
+    private String phone;
+
+    @Column
+    @Length(min = 3)
+    private String addressStreet;
+
+    @Column
+    @Length(min = 3)
+    private String addressCity;
+
+    @Column
+    @Length(min = 3)
+    private String addressZip;
+
+    @Column
+    private String addressState;
+
+    @Column()
+    private String addressCountry;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column
+    private Timestamp updatedAt;
+}
