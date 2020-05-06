7ec10ee9fd53996727d2597ac100d3eed00d56c8
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-05-06 13:33:13 +0200

    Add db connection and data for users.

---
+++ src/main/java/jrs/mystorage/entity/User.java
@@ -0,0 +1,23 @@
+package jrs.mystorage.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID userId;
+
+    private String email;
+    private String password;
+}
---
+++ src/main/resources/sql_data/data.sql
@@ -0,0 +1,4 @@
+-- CREATE USERS password: zaq1@WSXcde3
+INSERT INTO users(user_id, email, password)
+VALUES ('02346256-44b2-4811-917a-6ad03c56036e', 'jan.szewczyk1997@gmail.com', '$2a$10$jXXKMhNsw/l7FFuzKsfHAOH.qv881bSrQF8L.pQ/2QprSU8jzwyxm'),
+    ('3272d037-bd1f-4cc2-8991-20587bb97048', 'jan.kowalski997@gmail.com', '$2a$10$jXXKMhNsw/l7FFuzKsfHAOH.qv881bSrQF8L.pQ/2QprSU8jzwyxm');
