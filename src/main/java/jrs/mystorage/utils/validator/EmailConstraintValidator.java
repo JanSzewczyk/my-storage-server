14cddb4ae6e17e90cac7709e64e5a359b22116da
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-05-07 22:45:30 +0200

    Create Email validation

--- src/main/java/jrs/mystorage/utils/validator/EmailConstraintValidator.java
+++ src/main/java/jrs/mystorage/utils/validator/EmailConstraintValidator.java
@@ -5,15 +5,15 @@ import jrs.mystorage.utils.validator.annotation.PasswordValid;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class PasswordConstraintValidator implements ConstraintValidator<PasswordValid, String> {
+public class EmailConstraintValidator implements ConstraintValidator<PasswordValid, String> {
 
-    public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    public static final String EMAIL_PATTERN = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
         if (password == null)
             return false;
 
-        return password.matches(PASSWORD_PATTERN);
+        return password.matches(EMAIL_PATTERN);
     }
 }
---
+++ src/main/java/jrs/mystorage/utils/validator/annotation/EmailValid.java
@@ -0,0 +1,25 @@
+package jrs.mystorage.utils.validator.annotation;
+
+import jrs.mystorage.utils.validator.EmailConstraintValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = EmailConstraintValidator.class)
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface EmailValid {
+
+    String message() default "Invalid email";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
