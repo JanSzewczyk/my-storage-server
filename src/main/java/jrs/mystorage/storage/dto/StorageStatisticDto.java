54465795aefb1258fad1c96a780346406ba42cfb
JanSzewczyk <jan.szewczyk1997@gmail.com>
2020-07-12 14:25:59 +0200

    Fix currency

--- src/main/java/jrs/mystorage/storage/dto/StorageStatisticDto.java
+++ src/main/java/jrs/mystorage/storage/dto/StorageStatisticDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Currency;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 public class StorageStatisticDto {
 
     private LocalDate date;
+    private Currency currency;
 
     private Double totalValue = 0.0;
     private Double storedValue = 0.0;
